59a8dca3b9b66e6ec2804bfb36518aaef87a9746
Ricardo <jrgastl@gmail.com>
2025-12-09 22:20:42 +0100

    fix: target group arns

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "blog_autoscaling" {
   min_size              = 1
   max_size              = 2
   vpc_zone_identifier   = module.blog_vpc.public_subnets
-  alb_target_group_arns = [
+  target_group_arns = [
     module.blog_alb.target_group_arns["ex-instance"]
   ]
   security_groups       = [module.blog_sg.security_group_id]
